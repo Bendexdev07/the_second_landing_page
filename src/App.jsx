ac02b1856361e75735ae74bac38bd487e5300778
Bendexdev07 <bendibabdou234@gmail.com>
2026-02-07 11:07:07 +0100

    hero-section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import Navbar from "/src/componets/navbar/navbar.jsx";
+import HeroSection from "/src/componets/heroSection/herosection.jsx";
 
 export default function App() { 
   return (
     <>
     <div>
       <Navbar />
+      <HeroSection />
+
     </div>
    
 
---
+++ src/componets/heroSection/herosection.jsx
@@ -0,0 +1,37 @@
+import "./herosection.css"; 
+export default function HeroSection() {
+    return (
+    <div className="hero-section">
+        <div className="hero-img">
+            <img src="src/assets/hero-image.png" alt="Hero Image" />
+
+        </div>
+        <div className="hero-txt">
+            <h1>krate design and</h1>
+            
+               <h1>motion studio</h1>
+
+
+
+        </div>
+
+
+
+
+
+
+    </div>
+
+
+
+
+
+
+
+
+    )
+
+
+
+
+}
