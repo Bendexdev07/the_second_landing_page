4c0201d56171953a1f603c65e4ca5a5ffd377dd1
Bendexdev07 <bendibabdou234@gmail.com>
2026-02-26 15:30:12 +0100

    adding gsap as a dependecie

---
+++ src/componets/recentWork/project.jsx
@@ -0,0 +1,15 @@
+import "./project.css"
+
+
+export default function Project({imgSrc,description,}){
+    <div className="project-conatiner">
+        <img src={imgSrc} alt="" />
+        <p className="description">{description}</p>
+    </div>
+
+
+
+
+
+
+}
---
+++ src/componets/recentWork/recentWork.jsx
@@ -0,0 +1,27 @@
+import "./recentWork.css"
+
+export default function RecentWork(){
+
+<div className="container">
+    <div className="tittle">
+        <img src="" alt="" />
+        <p>Recent work</p>
+    </div>
+    <div className="grid">
+
+
+
+    </div>
+
+
+
+
+
+
+</div>
+
+
+
+
+
+}
